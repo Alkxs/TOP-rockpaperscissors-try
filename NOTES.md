7a6261a7007de9477ec12ad75d4c226592d05b09
Alex Lauri <alex.mav.80@gmail.com>
2021-11-10 21:00:35 +0100

    first draft final version

--- NOTES.md
+++ NOTES.md
@@ -1,8 +1,18 @@
-set 1 player choices
+<!-- set 1 player choices
 set pc choices
 choose a number from 0 to 2
 connect each number with a choice
 generate random number 0 to 2 for pc
 link number with a choice
 determine the winner
-update score of the winner
+update score of the winner -->
+
+1. Layout header
+2. Layout buttons
+3. Layout for the contest
+4. create click event for the 3 buttons
+5. when clicking the button hide layout buttons and show the actual winner
+6. Figure out what the computer is going to pick
+7. compare results and figure out who wins
+
+. Update table score
--- app.js
+++ app.js
@@ -1,18 +1,120 @@
 'use strict';
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+  //start the game
+  let main = document.querySelector('.main');
 
-let computerPlay = function () {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-  if (randomNumber === 1) {
-    console.log('rock');
-  } else if (randomNumber === 2) {
-    console.log('scissors');
-  } else {
-    console.log('paper');
-  }
+  let battle = document.querySelector('.battle');
+  let userChoiceDisplay = document.querySelector('.battleUserImg');
+  let pcChoiceDisplay = document.querySelector('.battlePcImg');
+  let newGame = document.querySelector('.play-again');
+  let resultDisplay = document.querySelector('.display-winner');
+
+  let playerScore = document.querySelector('.userscore');
+  let computerScore = document.querySelector('.pcscore');
+
+  let userChoice;
+  let choicePc;
+
+  const playMatch = () => {
+    let possibleChoices = document.querySelectorAll('.hand');
+    //handle click event
+    possibleChoices.forEach((possibleChoice) =>
+      possibleChoice.addEventListener('click', () => {
+        userChoice = possibleChoice.getAttribute('id');
+        //update img of user choice
+        if (possibleChoice === paper) {
+          userChoiceDisplay.src = `images/Paper.png`;
+        } else if (possibleChoice === rock) {
+          userChoiceDisplay.src = `images/Rock.png`;
+        } else {
+          userChoiceDisplay.src = `images/Scissors.png`;
+        }
+        GenerateComputerChoice();
+        compareHands();
+        //reset
+        main.classList.add('fadeout');
+        main.style.transition = '0.2s';
+        battle.classList.remove('fadein');
+        battle.style.transition = '0.2s';
+        //change display
+        newGame.addEventListener('click', () => {
+          main.classList.remove('fadeout');
+          main.style.transition = '0.2s';
+          battle.classList.add('fadein');
+          battle.style.transition = '0.2s';
+        });
+      })
+    );
+  };
+
+  //  Generate pc choice
+  const GenerateComputerChoice = () => {
+    //pc options
+    let optionsPc = ['paper', 'rock', 'scissors'];
+    //Generate pc number
+    let numberPc = Math.floor(Math.random() * optionsPc.length);
+    // Generate pc choice
+    choicePc = optionsPc[numberPc];
+    pcChoiceDisplay.src = `/images/${choicePc}.png`;
+  };
+
+  const compareHands = () => {
+    //check for a draw
+    if (userChoice === choicePc) {
+      resultDisplay.innerHTML = 'DRAW';
+      return;
+    }
+    //check for rock
+    if (userChoice === 'rock') {
+      if (choicePc === 'scissors') {
+        resultDisplay.innerHTML = 'YOU WIN';
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        resultDisplay.innerHTML = 'YOU LOSE';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //check for paper
+    if (userChoice === 'paper') {
+      if (choicePc === 'rock') {
+        resultDisplay.innerHTML = 'YOU WIN';
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        resultDisplay.innerHTML = 'YOU LOSE';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //check for scissors
+    if (userChoice === 'scissors') {
+      if (choicePc === 'paper') {
+        resultDisplay.innerHTML = 'YOU WIN';
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        resultDisplay.innerHTML = 'YOU LOSE';
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
+  const updateScore = () => {
+    playerScore.innerHTML = pScore;
+    computerScore.innerHTML = cScore;
+  };
+  // execute all other functions
+  playMatch();
 };
-
-computerPlay();
-
-let playGame = function (player, pc) {
-    if(player === 1 )
-};
+// execute the main function
+game();
